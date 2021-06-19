ba347a1cd05806538fe413d945ddcd9148a9a3e5
githrahat <86165666+githrahat@users.noreply.github.com>
2021-06-20 00:26:39 +0600

    love_website_uploaded

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    
+    $(".nav-bar").sticky({topSpacing:0});
+
+  //  Owl-carousel Slide
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      autoplay:true, 
+      animateIn: "fadeIn",
+      animateOut: "fadeOut",
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
+
+});
